096aa00d231836aaacdae076afbf36c697950f0b
Pranith <saikotti2@gmail.com>
2026-03-26 09:00:03 +0530

    feat: Initial commit

---
+++ src/main/java/com/appservice/DeployAppserviceApplication.java
@@ -0,0 +1,13 @@
+package com.appservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployAppserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployAppserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appservice/DeployAppserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployAppserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
